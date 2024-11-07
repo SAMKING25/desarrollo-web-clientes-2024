9e527af6a4c5b3b54957cbc71b8cd72c9f7af72a
Samuel Trujillo <stg0008@alu.medac.es>
2024-11-07 12:31:24 +0100

    ejercicio 6 sin terminar

--- relacion 02 - DOM/JS/ejercicio6.js
+++ relacion 02 - DOM/JS/ejercicio6.js
@@ -1,7 +1,14 @@
 let btnColor = document.querySelectorAll('button');
-btnColor.addEventListener('click', cambiarColor);
-
-switch (btnColor.value){
-    case "red":
-        btnColor.style.color = "red"
-}
+btnColor.addEventListener('click', function(){
+    for(let i=0; i<btnColor.length-1; i++){
+        switch (btnColor[i].value){
+            case "red":
+                document.body.style.backgroundColor = "red"
+                break;
+            case "yellow":
+                document.body.style.backgroundColor = yellow
+                break;
+        }
+    }
+    
+});
